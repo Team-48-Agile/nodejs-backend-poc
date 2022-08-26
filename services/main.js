a127272e045c28902f6f458e5f42450ad6b5268f
Lucas Viola <lucasmatzenbacher@gmail.com>
2022-08-26 16:47:43 -0300

    Replaces if statement by switch case

--- services/main.js
+++ services/main.js
@@ -23,69 +23,43 @@ const convertTextUsingTextVide = (text, sep, fixationPoints) => {
 }
 
 const convertFile = async (fileData, fileType, filePath, {sep: sep, fixation: fixation}) => {
+    let textWithBionic;
 
-    // TXT files
-    if(fileType == '.txt'){
-        const textFromTxt = await fileReader.readText(fileData)
+    switch (fileType) {
+        case '.txt':
+            const textFromTxt = await fileReader.readText(fileData)
+            console.log("Text to be converted with text-vide: ",  textFromTxt, " with a fixation of: ", fixation);
+            textWithBionic = convertTextUsingTextVide( textFromTxt, sep, fixation)
+            return {text: textFromTxt, textWithBionic, fixation};
 
-        console.log("Text to be converted with text-vide: ",  textFromTxt, " with a fixation of: ", fixation);
+        case '.pdf':
+            const textFromPdf = await fileReader.readPdf(fileData);
+            console.log("Text to be converted with text-vide: ", textFromPdf, " with a fixation of: ", fixation);
+            textWithBionic = convertTextUsingTextVide(textFromPdf, sep, fixation)
+            return {text: textFromPdf, textWithBionic, fixation};
 
-        const textWithBionic = convertTextUsingTextVide( textFromTxt, sep, fixation)
+        case '.docx':
+            const textFromDocs = await fileReader.readDocx(fileData);
+            console.log("Text to be converted with text-vide: ", textFromDocs, " with a fixation of: ", fixation);
+            textWithBionic = convertTextUsingTextVide(textFromDocs, sep, fixation)
+            return {text: textFromDocs, textWithBionic, fixation};
 
-        return {text: textFromTxt, textWithBionic, fixation};
+        case '.rtf':
+            const textFromRtf = await fileReader.readRtf(filePath);
+            console.log("Text to be converted with text-vide: ", textFromRtf, " with a fixation of: ", fixation);
+            textWithBionic = convertTextUsingTextVide(textFromRtf, sep, fixation)
+            return {text: textFromRtf, textWithBionic, fixation};
+
+        case '.html':
+            const text = await fileReader.readHtml(fileData);
+            console.log("Text to be converted with text-vide: ", text, " with a fixation of: ", fixation);
+            textWithBionic = convertTextUsingTextVide(text, sep, fixation)
+            return {text, textWithBionic, fixation};
+
+        default:
+            throw new Error('Error converting file: ' + fileType + ' not supported');
     }
 
-    // PDF files
-    else if(fileType == '.pdf'){
-        const textFromPdf = await fileReader.readPdf(fileData);
-
-        console.log("Text to be converted with text-vide: ", textFromPdf, " with a fixation of: ", fixation);
-
-        const textWithBionic = convertTextUsingTextVide(textFromPdf, sep, fixation)
-
-        return {text: textFromPdf, textWithBionic, fixation};
-    }
-
-    // DOCX files
-    else if(fileType == '.docx'){
-        const textFromDocs = await fileReader.readDocx(fileData);
-
-        console.log("Text to be converted with text-vide: ", textFromDocs, " with a fixation of: ", fixation);
-
-        const textWithBionic = convertTextUsingTextVide(textFromDocs, sep, fixation)
-
-        return {text: textFromDocs, textWithBionic, fixation};
-    }
-
-    // RTF files
-    else if(fileType == '.rtf'){
-        const textFromRtf = await fileReader.readRtf(filePath)
-            .catch((err) => {
-               console.log(err);
-            });
-
-        console.log("Text to be converted with text-vide: ", textFromRtf, " with a fixation of: ", fixation);
-
-        const textWithBionic = convertTextUsingTextVide(textFromRtf, sep, fixation)
-
-        return {text: textFromRtf, textWithBionic, fixation};
-    }
-
-    // HTML files
-    else if(fileType == '.html'){
-
-        const text = await fileReader.readHtml(fileData);
-
-        console.log("Text to be converted with text-vide: ", text, " with a fixation of: ", fixation);
-
-        const textWithBionic = convertTextUsingTextVide(text, sep, fixation)
-
-        return {text, textWithBionic, fixation};
-    }
-
-    else{
-        console.error(fileType + " is not supported!");
-    }
 }
 
 module.exports = {convertText, convertTextUsingTextVide, convertFile};
