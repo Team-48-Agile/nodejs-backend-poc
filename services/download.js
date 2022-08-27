2ac3550d1176520d6e8eb994d5660579730f5fab
Robert Allsop <robertallsop51@hotmail.co.uk>
2022-08-27 21:31:39 +0100

    Removed unused file

--- services/download.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.open('textfile.txt', 'w', function(err, file){
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log("File created");
-})
