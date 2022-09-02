d917d39d7ab26e3325e24a30cf49e858a44ea9ea
Lucas Viola <lucasmatzenbacher@gmail.com>
2022-09-02 15:25:10 -0300

    Extracts service to download file

--- routes/main.js
+++ routes/main.js
@@ -71,81 +71,12 @@ module.exports = function (app) {
         });
     });
     
-    app.post("/download", (req, res) => {
+    app.post("/download", async (req, res) => {
 
         const bionicText = req.body['bionicText'];
         const filename = req.body['filename'];
-        const fileType = req.body['fileType'];     
-        
-        console.log(bionicText);
+        const fileType = req.body['fileType'];
 
-        const t = filename + "." + fileType;
-        
-
-        const fs = require('fs');
-
-        fs.open(t, 'w', function(err, file){
-            if(err){
-                console.error(err);
-                return;
-            }
-            console.log("file created");
-        })
-
-        console.log(fileType);
-
-        if(fileType == "txt" || fileType == "html"){
-            
-            fs.writeFile(t, bionicText, function(err){
-                if(err){
-                    console.error(err);
-                    return;
-                }
-                console.log("text written");
-            });
-        }
-        if(fileType == "docx"){
-            const HTMLtoDOCX = require('html-to-docx');
-            
-            const test = "<p>" + bionicText + "</p>";
-            console.log(test);
-
-            (async() => {
-                const fileBuffer = await HTMLtoDOCX(test);
-
-                fs.writeFile(t, fileBuffer, function(err){
-                    if(err){
-                        console.error(err);
-                        return;
-                    }
-                    console.log("text written");
-                });
-            })();
-        }
-        if(fileType == "pdf"){
-            const html_to_pdf = require('html-pdf-node');
-            
-            const test = "<p>" + bionicText + "</p>";
-            console.log(test);
-
-            options = {format: 'A4'};
-            file = { content: test};
-
-            html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-                fs.writeFile(t, pdfBuffer, function(err){
-                    if(err){
-                        console.error(err);
-                        return;
-                    }
-                    console.log("text written");
-                });
-            })
-        } 
-        if(fileType == "rtf"){
-            const htmlToRtf = require('html-to-rtf');
-            const test = "<p>" + bionicText + "</p>";
-            htmlToRtf.saveRtfInFile(t, htmlToRtf.convertHtmlToRtf(test));
-        }      
-
+        await bionicReaderService.downloadFile(bionicText, filename, fileType);
     });    
 }
